28574fe01106ffafccf6d1d96c9fb8388daf568a
Tushar7337 <tushar.shingde@thinkitive.com>
2023-08-10 19:57:41 +0530

    Adding multiple ways for POST method like POJO,External JSON and HashMap
    POJO Clas

---
+++ src/test/java/com/restassured/Pages/POJORequest.java
@@ -0,0 +1,68 @@
+package com.restassured.Pages;
+
+public class POJORequest {
+
+    String firstName;
+    String lastName;
+    String countryCode;
+    String phoneNumber;
+    String email;
+    String dob;
+    String timeZone;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+}
